1645bfc34fc22719d5960833b38a4780de4cba7c
Antonia Blair <antoniablair@gmail.com>
2015-06-19 00:39:38 -0400

    Get karma working locally

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'vendor/lodash/lodash.js',
-      'app/**/*.js',
+      'spec/app/**/*.js',
       'spec/app/my_world_service_spec.js'
     ],
 
@@ -24,7 +24,6 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
